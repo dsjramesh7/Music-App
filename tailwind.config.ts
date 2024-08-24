3e019622b663b5666ae998a84da5d408bbab61ff
dsjramesh7 <rameshdussa07@gmail.com>
2024-08-25 00:43:14 +0530

    Hero section done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,33 @@
 import type { Config } from "tailwindcss";
 
+// Utility to flatten the color palette
+const flattenColorPalette = (colors: Record<string, unknown>) => {
+  return Object.entries(colors).reduce((acc, [color, value]) => {
+    if (typeof value === "object" && value !== null) {
+      Object.entries(value).forEach(([shade, shadeValue]) => {
+        if (typeof shadeValue === "string") {
+          acc[`${color}-${shade}`] = shadeValue;
+        }
+      });
+    } else if (typeof value === "string") {
+      acc[color] = value;
+    }
+    return acc;
+  }, {} as Record<string, string>);
+};
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,13 +37,30 @@ const config: Config = {
   darkMode: "class",
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        spotlight: {
+          "0%": { opacity: "0", transform: "translate(-72%, -62%) scale(0.5)" },
+          "100%": { opacity: "1", transform: "translate(-50%,-40%) scale(1)" },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
 export default config;
